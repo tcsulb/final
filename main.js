5d2de6b9c2067d27225a0f281d9a28d983ca430c
Tianna <tianna.hydro@csulb.edu>
2020-05-12 21:25:44 -0700

    step 2- fucntions

--- main.js
+++ main.js
@@ -4,5 +4,20 @@ const num2 = parseInt(document.getElementById('num2').value);
 document.getElementById("inputs").addEventListener("submit", validateInputs);
 
 function validateInputs() {
-    
+    let inputs = document.querySelector('inputs');
+    let num1 = parseInt(inputs.getElementById('num1').value);
+    let num2 = parseInt(inputs.getElementById('num2').value);
+
+    if (num1 < 2 || num1 > 100) {
+ 
+        result = "Number 1 input is not a valid number";
+
+    }
+    else if (num2 < 2 || num2 > 100) {
+   
+        result = "Number 2 input is not a valid number";
+
+        
+    }
+    return result;
 }
