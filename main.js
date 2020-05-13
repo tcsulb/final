e71975599f544e374650dbfa0171d9a0f12db184
Tianna <tianna.hydro@csulb.edu>
2020-05-12 21:12:58 -0700

    step 2 - event lustteners

---
+++ main.js
@@ -0,0 +1,8 @@
+const num1 = parseInt(document.getElementById('num1').value);
+const num2 = parseInt(document.getElementById('num2').value);
+
+document.getElementById("inputs").addEventListener("submit", validateInputs);
+
+function validateInputs() {
+    
+}
